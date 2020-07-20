0f11546932cd31b035cbd81957870bb10e0ecc55
srlechuga18 <aledeleon222@hotmail.com>
2020-07-20 10:07:21 -0500

    imports modulos

---
+++ operaciones.py
@@ -0,0 +1,15 @@
+def suma(a, b):
+	return a+b
+
+def resta(a, b):
+	return a-b
+
+def producto(a, b):
+	return a*b
+
+
+# Funciones para probar el módulo
+if __name__ == "__main__":
+    print("La suma de 5 y 10 es", suma(5, 10))
+    print("La resta de -9 y 8 es", resta(-9, 8))
+    print("El producto de 3 y -6 es", producto(3, -6))
---
+++ programa.py
@@ -0,0 +1,5 @@
+from operaciones import producto, suma
+
+#print("4*6=", operaciones.producto(4, 6))
+print("4*6=", producto(4, 6))
+print("5+3=", suma(5, 3))
